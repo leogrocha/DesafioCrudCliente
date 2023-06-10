1eb5b4e5cf65caa2803cf0550019609eed3eaea4
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-06-10 14:20:09 -0300

    Initial commit

---
+++ src/test/java/com/devsuperior/DesafioCrudCliente/DesafioCrudClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DesafioCrudCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
